20f0e6b66caae91efe5984dbdd9737fe2ff37110
(no author) <(no author)@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2004-05-10 13:20:52 +0000

    This commit was manufactured by cvs2svn to create tag 'r2_3_3'.
    git-svn-id: svn://pqxx.org/libpqxx@470 c3353b84-d008-0410-9ed9-b55c4f7fa7e8

---
+++ tags/r2_3_3/libpqxx/src/robusttransaction.cxx
@@ -0,0 +1,246 @@
+/*-------------------------------------------------------------------------
+ *
+ *   FILE
+ *	robusttransaction.cxx
+ *
+ *   DESCRIPTION
+ *      implementation of the pqxx::robusttransaction class.
+ *   pqxx::robusttransaction is a slower but safer transaction class
+ *
+ * Copyright (c) 2002-2004, Jeroen T. Vermeulen <jtv@xs4all.nl>
+ *
+ * See COPYING for copyright license.  If you did not receive a file called
+ * COPYING with this source code, please notify the distributor of this mistake,
+ * or contact the author.
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "pqxx/compiler.h"
+
+#include <stdexcept>
+
+#include "pqxx/connection_base"
+#include "pqxx/result"
+#include "pqxx/robusttransaction"
+
+
+using namespace PGSTD;
+
+
+#define SQL_COMMIT_WORK 	"COMMIT"
+#define SQL_ROLLBACK_WORK 	"ROLLBACK"
+
+pqxx::basic_robusttransaction::basic_robusttransaction(connection_base &C, 
+	const string &IsolationLevel,
+	const string &TName) :
+  dbtransaction(C, 
+      IsolationLevel, 
+      TName, 
+      "robusttransaction<"+IsolationLevel+">"),
+  m_ID(oid_none),
+  m_LogTable()
+{
+  m_LogTable = string("PQXXLOG_") + conn().username();
+}
+
+
+pqxx::basic_robusttransaction::~basic_robusttransaction()
+{
+}
+
+
+void pqxx::basic_robusttransaction::do_begin()
+{
+  start_backend_transaction();
+
+  try
+  {
+    CreateTransactionRecord();
+  }
+  catch (const exception &)
+  {
+    // The problem here *may* be that the log table doesn't exist yet.  Create
+    // one, start a new transaction, and try again.
+    try { DirectExec(SQL_ROLLBACK_WORK, 2); } catch (const exception &e) {}
+    CreateLogTable();
+    start_backend_transaction();
+    CreateTransactionRecord();
+  }
+}
+
+
+
+void pqxx::basic_robusttransaction::do_commit()
+{
+  const IDType ID = m_ID;
+
+  if (ID == oid_none) 
+    throw logic_error("libpqxx internal error: transaction " 
+		      "'" + name() + "' " 
+		     "has no ID");
+
+  // Check constraints before sending the COMMIT to the database to reduce the
+  // work being done inside our in-doubt window.
+  //
+  // This may not an entirely clear-cut 100% obvious unambiguous Good Thing.  If
+  // we lose our connection during the in-doubt window, it may be better if the
+  // transaction didn't actually go though, and having constraints checked first
+  // theoretically makes it more likely that it will once we get to that stage.
+  // However, it seems reasonable to assume that most transactions will satisfy
+  // their constraints.  Besides, the goal of robusttransaction is to weed out
+  // indeterminacy, rather than to improve the odds of desirable behaviour when
+  // all bets are off anyway.
+  DirectExec("SET CONSTRAINTS ALL IMMEDIATE");
+
+  // Here comes the critical part.  If we lose our connection here, we'll be 
+  // left clueless as to whether the backend got the message and is trying to
+  // commit the transaction (let alone whether it will succeed if so).  This
+  // case requires some special handling that makes robusttransaction what it 
+  // is.
+  try
+  {
+    DirectExec(SQL_COMMIT_WORK);
+  }
+  catch (const exception &e)
+  {
+    // TODO: Can we limit this handler to broken_connection exceptions?
+    m_ID = oid_none;
+    if (!conn().is_open())
+    {
+      // We've lost the connection while committing.  We'll have to go back to
+      // the backend and check our transaction log to see what happened.
+      process_notice(e.what() + string("\n"));
+
+      // See if transaction record ID exists; if yes, our transaction was 
+      // committed before the connection went down.  If not, the transaction 
+      // was aborted.
+      bool Exists;
+      try
+      {
+        Exists = CheckTransactionRecord(ID);
+      }
+      catch (const exception &f)
+      {
+	// Couldn't reconnect to check for transaction record.  We're still in 
+	// doubt as to whether the transaction was performed.
+	process_notice(string(f.what()) + "\n");
+
+        const string Msg = "WARNING: "
+		    "Connection lost while committing transaction "
+		    "'" + name() + "' (oid " + to_string(ID) + "). "
+		    "Please check for this record in the "
+		    "'" + m_LogTable + "' table.  "
+		    "If the record exists, the transaction was executed. "
+		    "If not, then it hasn't.";
+
+        process_notice(Msg + "\n");
+        throw in_doubt_error(Msg);
+      }
+ 
+      // Transaction record is gone, so all we have is a "normal" transaction 
+      // failure.
+      if (!Exists) throw;
+    }
+    else
+    {
+      // Commit failed--probably due to a constraint violation or something 
+      // similar.  But we're still connected, so no worries from a consistency
+      // point of view.
+ 
+      // Try to delete transaction record ID, if it still exists (although it 
+      // really shouldn't)
+      DeleteTransactionRecord(ID);
+      throw;
+    }
+  }
+
+  m_ID = oid_none;
+  DeleteTransactionRecord(ID);
+}
+
+
+void pqxx::basic_robusttransaction::do_abort()
+{
+  m_ID = oid_none;
+
+  // Rollback transaction.  Our transaction record will be dropped as a side
+  // effect, which is what we want since "it never happened."
+  DirectExec(SQL_ROLLBACK_WORK);
+}
+
+
+// Create transaction log table if it didn't already exist
+void pqxx::basic_robusttransaction::CreateLogTable()
+{
+  // Create log table in case it doesn't already exist.  This code must only be 
+  // executed before the backend transaction has properly started.
+  const string CrTab = "CREATE TABLE " + m_LogTable +
+	               "("
+	               "name VARCHAR(256), "
+	               "date TIMESTAMP"
+	               ")";
+
+  try { DirectExec(CrTab.c_str()); } catch (const exception &) { }
+}
+
+
+void pqxx::basic_robusttransaction::CreateTransactionRecord()
+{
+  const string Insert = "INSERT INTO " + m_LogTable + " "
+	                "(name, date) "
+			"VALUES "
+	                "(" +
+			(name().empty() ? "null" : "'"+sqlesc(name())+"'") +
+			", "
+	                "CURRENT_TIMESTAMP"
+	                ")";
+
+  m_ID = DirectExec(Insert.c_str()).inserted_oid();
+
+  if (m_ID == oid_none) 
+    throw runtime_error("Could not create transaction log record");
+}
+
+
+void pqxx::basic_robusttransaction::DeleteTransactionRecord(IDType ID) throw ()
+{
+  if (ID == oid_none) return;
+
+  try
+  {
+    // Try very, very hard to delete record.  Specify an absurd retry count to 
+    // ensure that the server gets a chance to restart before we give up.
+    const string Del = "DELETE FROM " + m_LogTable + " "
+	               "WHERE oid=" + to_string(ID);
+
+    DirectExec(Del.c_str(), 20);
+
+    // Now that we've arrived here, we're almost sure that record is quite dead.
+    ID = oid_none;
+  }
+  catch (const exception &)
+  {
+  }
+
+  if (ID != oid_none) try
+  {
+    process_notice("WARNING: "
+	           "Failed to delete obsolete transaction record with oid " + 
+		   to_string(ID) + " ('" + name() + "'). "
+		   "Please delete it manually.  Thank you.\n");
+  }
+  catch (const exception &)
+  {
+  }
+}
+
+
+// Attempt to establish whether transaction record with given ID still exists
+bool pqxx::basic_robusttransaction::CheckTransactionRecord(IDType ID)
+{
+  const string Find = "SELECT oid FROM " + m_LogTable + " "
+	              "WHERE oid=" + to_string(ID);
+
+  return !DirectExec(Find.c_str(), 20).empty();
+}
+
---
+++ tags/r2_3_3/libpqxx/src/tablewriter.cxx
@@ -0,0 +1,191 @@
+/*-------------------------------------------------------------------------
+ *
+ *   FILE
+ *	tablewriter.cxx
+ *
+ *   DESCRIPTION
+ *      implementation of the pqxx::tablewriter class.
+ *   pqxx::tablewriter enables optimized batch updates to a database table
+ *
+ * Copyright (c) 2001-2004, Jeroen T. Vermeulen <jtv@xs4all.nl>
+ *
+ * See COPYING for copyright license.  If you did not receive a file called
+ * COPYING with this source code, please notify the distributor of this mistake,
+ * or contact the author.
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "pqxx/compiler.h"
+
+#include "pqxx/tablereader"
+#include "pqxx/tablewriter"
+#include "pqxx/transaction"
+
+using namespace PGSTD;
+
+
+pqxx::tablewriter::tablewriter(transaction_base &T, 
+    const string &WName,
+    const string &Null) :
+  tablestream(T, WName, Null, "tablewriter"),
+  m_PendingLine()
+{
+  setup(T, WName);
+}
+
+
+pqxx::tablewriter::~tablewriter() throw ()
+{
+  try
+  {
+    writer_close();
+  }
+  catch (const exception &e)
+  {
+    reg_pending_error(e.what());
+  }
+}
+
+
+void pqxx::tablewriter::setup(transaction_base &T,
+    const string &WName,
+    const string &Columns)
+{
+  T.BeginCopyWrite(WName, Columns);
+  register_me();
+}
+
+
+pqxx::tablewriter &pqxx::tablewriter::operator<<(pqxx::tablereader &R)
+{
+  string Line;
+  // TODO: Can we do this in binary mode? (Might require protocol version check)
+  while (R.get_raw_line(Line))
+  {
+    if (!Line.empty() && (Line[Line.size()-1]=='\n')) Line.erase(Line.size()-1);
+    WriteRawLine(Line);
+  }
+
+  return *this;
+}
+
+
+void pqxx::tablewriter::WriteRawLine(const string &Line)
+{
+  /* We use an nonblocking write protocol.  If there is currently no space to
+   * write a line to our socket, the tablewriter stores the line being written
+   * in a single-line buffer of its own.  This allows the client program to run
+   * a little ahead of the socket's transfer capability, hopefully overlapping 
+   * some client-side processing with some server-side or networking time.  In
+   * principle the tablewriter's buffer could be extended to allow for multiple
+   * lines of discrepancy.
+   * This does mean that, before writing a new line, we must check for a pending
+   * line and send that through first.
+   */
+  flush_pending();
+  if (!m_Trans.WriteCopyLine(Line, true)) m_PendingLine = Line;
+}
+
+
+void pqxx::tablewriter::flush_pending()
+{
+  if (!m_PendingLine.empty())
+  {
+    if (!m_Trans.WriteCopyLine(m_PendingLine))
+      throw logic_error("libpqxx internal error: "
+	  "writing pending line in async mode");
+#ifdef PQXX_HAVE_STRING_CLEAR
+    m_PendingLine.clear();
+#else
+    m_PendingLine.resize(0);
+#endif
+  }
+}
+
+
+void pqxx::tablewriter::complete()
+{
+  writer_close();
+}
+
+
+void pqxx::tablewriter::writer_close()
+{
+  flush_pending();
+  if (!is_finished())
+  {
+    base_close();
+    try 
+    { 
+      m_Trans.EndCopyWrite(); 
+    } 
+    catch (const exception &) 
+    {
+      try { base_close(); } catch (const exception &) {}
+      throw;
+    }
+  }
+}
+
+
+namespace
+{
+inline char escapechar(char i)
+{
+  char r = '\0';
+  switch (i)
+  {
+    case 8:	r='b';	break;	// backspace
+    case 11:	r='v';	break;	// vertical tab
+    case 12:	r='f';	break;	// form feed
+    case '\n':	r='n';	break;	// newline
+    case '\t':	r='t';	break;	// tab
+    case '\r':	r='r';	break;	// carriage return
+    case '\\':	r='\\';	break;	// backslash
+  }
+  return r;
+}
+
+inline bool ishigh(char i)
+{
+  return (i & 0x80) != 0;
+}
+
+inline char tooctdigit(unsigned int i, int n)
+{
+  return '0' + ((i>>(3*n)) & 0x07);
+}
+} // namespace
+
+
+string pqxx::tablewriter::Escape(const string &S)
+{
+  if (S.empty()) return S;
+
+  string R;
+  R.reserve(S.size()+1);
+
+  for (string::const_iterator j = S.begin(); j != S.end(); ++j)
+  {
+    const char c = *j;
+    const char e = escapechar(c);
+    if (e)
+    {
+      R += '\\';
+      R += e;
+    }
+    else if (ishigh(c))
+    {
+      R += '\\';
+      unsigned char u=c;
+      for (int n=2; n>=0; --n) R += tooctdigit(u,n);
+    }
+    else
+    {
+      R += c;
+    }
+  }
+  return R;
+}
+
+
