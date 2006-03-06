0a659920778106192f698f441f75c877f491f187
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2006-03-06 19:33:21 +0000

    Released 2.6.5
    git-svn-id: svn://pqxx.org/libpqxx@1035 c3353b84-d008-0410-9ed9-b55c4f7fa7e8

---
+++ tags/2.6.5/autogen.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+# Run this to generate all the initial makefiles, etc.
+# Set CONFIG_ARGS to the argument list you wish to pass to configure
+
+set -e
+
+# Run in most basic of locales to avoid performance overhead (and risk of bugs)
+# involved in localization, encoding issues etc.  We only do ASCII here.
+export LC_ALL=C
+
+ver=""
+for amv in "1.6" "1.7" "1.8" "1.9" ; do
+	if which "automake-$amv" >/dev/null 2>&1; then
+		ver="-$amv"
+	fi
+done
+
+# The VERSION file defines our versioning
+PQXXVERSION=`grep '\<PQXXVERSION\>' VERSION | sed -e 's/^[[:space:]A-Z_]*//' -e 's/[[:space:]]*#.*$//'`
+echo "libpqxx version $PQXXVERSION"
+
+# Generate configure.ac based on current version numbers
+sed -e "s/@PQXXVERSION@/$PQXXVERSION/g" configure.ac.in >configure.ac
+
+# Generate test/Makefile.am
+./tools/maketestam.pl test >test/Makefile.am
+
+# Generate Windows makefiles (adding carriage returns to make it MS-DOS format)
+./tools/maketestvcmak.pl test | sed -e 's/$/\r/' >win32/test.mak
+./tools/makevcmake.pl src | sed -e 's/$/\r/' >win32/libpqxx.mak
+./tools/makemingwmak.pl src | sed -e 's/$/\r/' >win32/MinGW.mak
+
+autoheader
+
+libtoolize --force --automake --copy
+aclocal${ver} -I . -I config/m4
+automake${ver} --verbose --add-missing --copy
+autoconf
+
+conf_flags="--enable-maintainer-mode $CONFIG_ARGS"
+if test -z "$NOCONFIGURE" ; then
+	echo Running $srcdir/configure $conf_flags "$@" ...
+	./configure $conf_flags "$@" \
+	&& echo Now type \`make\' to compile $PKG_NAME || exit 1
+else
+	echo Skipping configure process.
+fi
+
---
+++ tags/2.6.5/include/pqxx/tablereader.hxx
@@ -0,0 +1,166 @@
+/*-------------------------------------------------------------------------
+ *
+ *   FILE
+ *	pqxx/tablereader.hxx
+ *
+ *   DESCRIPTION
+ *      definition of the pqxx::tablereader class.
+ *   pqxx::tablereader enables optimized batch reads from a database table
+ *   DO NOT INCLUDE THIS FILE DIRECTLY; include pqxx/tablereader instead.
+ *
+ * Copyright (c) 2001-2006, Jeroen T. Vermeulen <jtv@xs4all.nl>
+ *
+ * See COPYING for copyright license.  If you did not receive a file called
+ * COPYING with this source code, please notify the distributor of this mistake,
+ * or contact the author.
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "pqxx/compiler-public.hxx"
+#include "pqxx/compiler-internal-pre.hxx"
+
+#include "pqxx/result"
+#include "pqxx/tablestream"
+
+/* Methods tested in eg. self-test program test001 are marked with "//[t1]"
+ */
+
+namespace pqxx
+{
+
+/// Efficiently pull data directly out of a table.
+/** A tablereader provides efficient read access to a database table.  This is
+ * not as flexible as a normal query using the result class however:
+ * <ul>
+ * <li> Can only dump tables, not views or arbitrary queries
+ * <li> Has no knowledge of metadata
+ * <li> Is unable to reorder, rename, omit or enrich fields
+ * <li> Does not support filtering of records
+ * </ul>
+ *
+ * On the other hand, it can read rows of data and transform them into any
+ * container or container-like object that supports STL back-inserters.  Since
+ * the tablereader has no knowledge of the types of data expected, it treats
+ * all fields as strings.
+ */
+class PQXX_LIBEXPORT tablereader : public tablestream
+{
+public:
+  tablereader(transaction_base &,
+      const PGSTD::string &Name,
+      const PGSTD::string &Null=PGSTD::string());			//[t6]
+
+  /// Read only the given sequence of columns
+  /** @since PostgreSQL backend 7.3
+   */
+  template<typename ITER>
+  tablereader(transaction_base &,
+      const PGSTD::string &Name,
+      ITER begincolumns,
+      ITER endcolumns);							//[t80]
+
+  template<typename ITER> tablereader(transaction_base &,
+      const PGSTD::string &Name,
+      ITER begincolumns,
+      ITER endcolumns,
+      const PGSTD::string &Null);					//[t80]
+
+  ~tablereader() throw ();						//[t6]
+
+  template<typename TUPLE> tablereader &operator>>(TUPLE &);		//[t8]
+
+  operator bool() const throw () { return !m_Done; }			//[t6]
+  bool operator!() const throw () { return m_Done; }			//[t6]
+
+  /// Read a line of raw, unparsed table data
+  /**
+   * @param Line Variable to hold the raw data line read from the table.
+   * @return Whether a line could be read
+   */
+  bool get_raw_line(PGSTD::string &Line);				//[t8]
+
+  template<typename TUPLE>
+  void tokenize(PGSTD::string, TUPLE &) const;				//[t8]
+
+  /// Finish stream action, check for errors, and detach from transaction
+  /** It is recommended that you call this function before the tablestream's
+   * destructor is run.  This function will check any final errors which may not
+   * become apparent until the transaction is committed otherwise.
+   *
+   * As an added benefit, this will free up the transaction while the
+   * tablestream object itself still exists.
+   */
+  virtual void complete();						//[t8]
+
+#ifdef PQXX_DEPRECATED_HEADERS
+  /// @deprecated Use get_raw_line() instead
+  bool GetRawLine(PGSTD::string &L) { return get_raw_line(L); }
+  /// @deprecated Use tokenize<>() instead
+  template<typename TUPLE> void Tokenize(PGSTD::string L, TUPLE &T) const
+  	{ tokenize(L, T); }
+#endif
+
+private:
+  void setup(transaction_base &T,
+      const PGSTD::string &RName,
+      const PGSTD::string &Columns=PGSTD::string());
+  void PQXX_PRIVATE reader_close();
+  PGSTD::string extract_field(const PGSTD::string &,
+      PGSTD::string::size_type &) const;
+
+  bool m_Done;
+};
+
+
+// TODO: Find meaningful definition of input iterator
+
+
+template<typename ITER> inline
+tablereader::tablereader(transaction_base &T,
+    const PGSTD::string &Name,
+    ITER begincolumns,
+    ITER endcolumns) :
+  namedclass(Name, "tablereader"),
+  tablestream(T, PGSTD::string()),
+  m_Done(true)
+{
+  setup(T, Name, columnlist(begincolumns, endcolumns));
+}
+
+template<typename ITER> inline
+tablereader::tablereader(transaction_base &T,
+    const PGSTD::string &Name,
+    ITER begincolumns,
+    ITER endcolumns,
+    const PGSTD::string &Null) :
+  namedclass(Name, "tablereader"),
+  tablestream(T, Null),
+  m_Done(true)
+{
+  setup(T, Name, columnlist(begincolumns, endcolumns));
+}
+
+
+template<typename TUPLE>
+inline void tablereader::tokenize(PGSTD::string Line, TUPLE &T) const
+{
+  PGSTD::back_insert_iterator<TUPLE> ins = PGSTD::back_inserter(T);
+
+  // Filter and tokenize line, inserting tokens at end of T
+  PGSTD::string::size_type here=0;
+  while (here < Line.size()) *ins++ = extract_field(Line, here);
+}
+
+
+template<typename TUPLE>
+inline tablereader &pqxx::tablereader::operator>>(TUPLE &T)
+{
+  PGSTD::string Line;
+  if (get_raw_line(Line)) tokenize(Line, T);
+  return *this;
+}
+
+
+} // namespace pqxx
+
+#include "pqxx/compiler-internal-post.hxx"
